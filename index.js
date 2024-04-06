3e5a9b7e513b32fe6d13587810864df4c3518cb2
Marcus Lo <marcusklo1010@gmail.com>
2024-04-05 20:15:27 -0600

    domcontentloaded complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+  const paragraph = document.getElementById('text');
+  paragraph.textContent = 'This is really cool!';
+});
+
+expect(document.querySelector('p').textContent).to.equal("This is really cool!");
